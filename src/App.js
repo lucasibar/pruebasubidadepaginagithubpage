ef695384b358551009cc6e44e0ce91d88b508bd7
lucasibar <lucas.ibar.soy.henry@gmail.com>
2024-05-24 23:05:45 -0300

    segundo cambio

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       <h1>PRIMER DEPLOY EN GITHUB</h1>
+      <h2>segundo cambio a ver si no fue coincidencia</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
