685a9f862f0591662391dddeb1e560968a2e6451
lucasibar <lucas.ibar.soy.henry@gmail.com>
2024-06-01 23:16:57 -0300

    a ver

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       <h1>PRIMER DEPLOY EN GITHUB</h1>
-      <h2>segundo cambio a ver si no fue coincidencia</h2>
+      <h2>bueno si funciona esto vemos de subir el resto de las cosas</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
